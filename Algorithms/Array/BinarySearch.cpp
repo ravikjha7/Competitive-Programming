f3d139cd5d4622dd7849fbf0740b16f4760c8edf
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2021-11-13 11:21:47 +0530

    Add : Comments Added in Binary Search Algorithms

--- Algorithms/Array/BinarySearch.cpp
+++ Algorithms/Array/BinarySearch.cpp
@@ -2,26 +2,34 @@
 using namespace std;
 typedef long long ll;
 
-int BinarySearch(int arr[],int key,int low,int high)
+int BinarySearch(int arr[],int key,int n)
 {
+    // Low points to the start of the array
+    // High points to the end of the array
+    int low = 0,high = n-1;
     while(low<=high)
     {
         int mid = low + (high-low)/2;
+        // If middle element is equal to required element , just return the index of the middle one
         if(arr[mid]==key)
         return mid;
+        // If middle element is greater than required element , then discard the right half of the array
         else if(arr[mid]>key)
         high = mid-1;
+        // If middle element is lesser than required element , then discard the left half of the array
         else
         low = mid+1;
     }
+    // If element is not found in the array , return -1
     return -1;
 }
 
 int main()
 {
     int arr[] = {1,3,7,8,99};
-    int key = 10;
+    int key = 7;
     int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<BinarySearch(arr,key,0,n)<<endl;
+    // Printing the index of the required element in the array
+    cout<<BinarySearch(arr,key,n)<<endl;
     return 0;
 }
