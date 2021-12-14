c07d9034e34696aa3ecec851e05178c3ee6d89d8
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2021-12-14 16:40:41 +0530

    Add : Problem 1 Round 758

---
+++ Contests/CodeForces/CodeForces Rounds/Round 758/findarray.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++) cout<<i+2<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
